0872fd8b6da11d26e3e199fdfbfd3ddc808d3bdc
Lianne Gill <lianne@winterwinds.io>
2021-02-03 16:05:40 -0700

    Updated skill with ros libary

--- __init__.py
+++ __init__.py
@@ -1,13 +1,15 @@
 from mycroft import MycroftSkill, intent_file_handler
-
+import rospy
 
 class RosPublisher(MycroftSkill):
     def __init__(self):
         MycroftSkill.__init__(self)
+        rospy.init_node('mycroft_node', log_level=rospy.DEBUG)
 
     @intent_file_handler('publisher.ros.intent')
     def handle_publisher_ros(self, message):
         self.speak_dialog('publisher.ros')
+        rospy.loginfo(message)
 
 
 def create_skill():
