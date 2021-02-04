75f7cc9ff0e49ee5c9ad3e3ed901a9ebf73fea37
Lianne Gill <lianne@winterwinds.io>
2021-02-04 09:47:58 -0700

    Changing from ROS1 to ROS2

--- __init__.py
+++ __init__.py
@@ -1,18 +1,24 @@
 from mycroft import MycroftSkill, intent_file_handler
-import rospy
+import rclpy
+from rclpy.node import Node
+
 from std_msgs.msg import String
 
 class RosPublisher(MycroftSkill):
     def __init__(self):
         MycroftSkill.__init__(self)
-        rospy.init_node('mycroft_node', log_level=rospy.DEBUG, disable_signals=True)
-        self.pub = rospy.Publisher("/mycroft", String, queue_size=1)
+        self.node = rclpy.create_node('mycroft_pub')
+        self.pub = self.node.create_publisher(String, "/mycroft", 1)
 
     @intent_file_handler('publisher.ros.intent')
     def handle_publisher_ros(self, message):
         self.speak_dialog('publisher.ros')
-        self.pub.publish(message.data.get('utterance'))
-
+        msg = String()
+        msg.data = message.data.get('utterance')
+        self.pub.publish(msg)
 
 def create_skill():
     return RosPublisher()
+
+
+
