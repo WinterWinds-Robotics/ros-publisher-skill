02ea08ebf08b89e8c925b60ff46c6088265ba076
Aaron Jones <aaron@winterwinds.io>
2021-02-03 17:09:55 -0700

    Publish to a mycroft topic.

--- __init__.py
+++ __init__.py
@@ -5,13 +5,14 @@ class RosPublisher(MycroftSkill):
     def __init__(self):
         MycroftSkill.__init__(self)
         rospy.init_node('mycroft_node', log_level=rospy.DEBUG, disable_signals=True)
+        self.pub = rospy.Publisher("/mycroft", String, queue_size=1)
 
     @intent_file_handler('publisher.ros.intent')
     def handle_publisher_ros(self, message):
         self.speak_dialog('publisher.ros')
+        self.pub(message)
         rospy.loginfo(message)
 
 
 def create_skill():
     return RosPublisher()
-
